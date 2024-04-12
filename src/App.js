a20d43a365ba09a05adb3560156060c34f9e8d20
LeonieNollevalle <leonie.nollevalle@gmail.com>
2024-04-12 17:13:08 +0200

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="w-screen h-screen overflow-hidden">
+    <div className="w-screen h-screen overflow-hidden mt-20">
      <iframe title="prototype" className="w-full " height="1200" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFNNFzuKyDFRze4opz0MDSr%2F%25F0%259F%2593%259A-%255BProjet-pro%255D-Delivery%3Fpage-id%3D1128%253A757%26type%3Ddesign%26node-id%3D1129-1894%26viewport%3D-2831%252C10%252C0.23%26t%3DRXeKo8235OYIM3cM-1%26scaling%3Dmin-zoom%26starting-point-node-id%3D1129%253A1894%26mode%3Ddesign" allowfullscreen='true'></iframe>    
     </div>
   );
