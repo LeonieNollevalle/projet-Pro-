5e1711d034e444f5c573d1d5b497d16dd5993cf2
LeonieNollevalle <leonie.nollevalle@gmail.com>
2024-04-12 17:02:42 +0200

    le qr code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen overflow-hidden">
+     <iframe title="prototype" className="w-full " height="1200" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FFNNFzuKyDFRze4opz0MDSr%2F%25F0%259F%2593%259A-%255BProjet-pro%255D-Delivery%3Fpage-id%3D1128%253A757%26type%3Ddesign%26node-id%3D1129-1894%26viewport%3D-2831%252C10%252C0.23%26t%3DRXeKo8235OYIM3cM-1%26scaling%3Dmin-zoom%26starting-point-node-id%3D1129%253A1894%26mode%3Ddesign" allowfullscreen='true'></iframe>    
     </div>
   );
 }
-
 export default App;
